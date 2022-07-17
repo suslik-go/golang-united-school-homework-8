cae85befafa9b47d490d6b790f56764e4f70d446
suslik <hasmik_margaryan@epam.com>
2022-07-17 10:18:53 -0700

    Initial solution

--- main.go
+++ main.go
@@ -1,16 +1,162 @@
 package main
 
 import (
+	"encoding/json"
+	"flag" //command line parsing
+	"fmt"
 	"io"
+	"log"
 	"os"
 )
 
-func Perform(args Arguments, writer io.Writer) error {
+type Arguments map[string]string
+
+type User struct {
+	Id    int
+	Email string
+	Age   int
 }
 
+//usage: `./main.go -operation «add» -item ‘{«id»: "1", «email»: «email@test.com», «age»: 23}’ -fileName «users.json»`
 func main() {
 	err := Perform(parseArgs(), os.Stdout)
 	if err != nil {
 		panic(err)
 	}
+}
+
+// Arguments fields: id, item, operation and fileName
+func Perform(args Arguments, writer io.Writer) error {
+
+	var user User
+	json.Unmarshal([]byte(args["item"]), &user)
+	fmt.Printf("Id: %d, Email: %s, Age: %d", user.Id, user.Email, user.Age)
+
+	switch {
+	case args["operation"] == "add":
+		add(user, args["fileName"])
+	case args["operation"] == "list":
+		writer.Write(list(args["fileName"]))
+	case args["operation"] == "findById":
+		findById(user, args["fileName"])
+	case args["operation"] == "remove":
+		remove(user, args["fileName"])
+	}
+	return nil
+}
+
+func parseArgs() Arguments {
+	var id = flag.String("id", "", "command id")
+	var userInfo = flag.String("item", "", "user information with this form '{«id»: '1', «email»: «email@test.com», «age»: 23}'")
+	var operation = flag.String("operation", "", "operation applied to provided user info")
+	var fileName = flag.String("fileName", "", "name of the file where info is being stored")
+
+	flag.Parse()
+
+	return Arguments{
+		"id":        *id,
+		"item":      *userInfo,
+		"operation": *operation,
+		"fileName":  *fileName,
+	}
+
+}
+
+func add(user User, fileName string) {
+	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// decode
+	var users []User
+	jsonDecodeErr := json.Unmarshal(data, &users)
+	if jsonDecodeErr != nil {
+		fmt.Println("error:", jsonDecodeErr)
+	}
+
+	users = append(users, user)
+
+	//encode
+	data, jsonEncodeErr := json.Marshal(users)
+	if jsonEncodeErr != nil {
+		fmt.Println("error:", jsonEncodeErr)
+	}
+
+	if _, err := file.Write(data); err != nil {
+		file.Close()
+		log.Fatal(err)
+	}
+
+	if err := file.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func list(fileName string) []byte {
+	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return data
+}
+
+func findById(user User, fileName string) User {
+	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// decode
+	var users []User
+	jsonDecodeErr := json.Unmarshal(data, &users)
+	if jsonDecodeErr != nil {
+		fmt.Println("error:", jsonDecodeErr)
+	}
+
+	for _, val := range users {
+		if val.Id == user.Id {
+			return user
+		}
+	}
+	return user
+}
+
+func remove(user User, fileName string) {
+	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// decode
+	var users []User
+	jsonDecodeErr := json.Unmarshal(data, &users)
+	if jsonDecodeErr != nil {
+		fmt.Println("error:", jsonDecodeErr)
+	}
+	var id int = int(user.Id)
+	users = append(users[:id], users[id+1:]...)
 }
